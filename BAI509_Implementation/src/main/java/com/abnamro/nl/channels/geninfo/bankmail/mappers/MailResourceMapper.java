fe5afdd18c56e3608cb58424971e8319aa07e2b3
Roger Janssen <roger.janssen@nl.abnamro.com>
2021-05-12 12:49:42 +0200

    remove no longer used mapping method after we started using the list-container classes that were already present but for some reason unused

--- BAI509_Implementation/src/main/java/com/abnamro/nl/channels/geninfo/bankmail/mappers/MailResourceMapper.java
+++ BAI509_Implementation/src/main/java/com/abnamro/nl/channels/geninfo/bankmail/mappers/MailResourceMapper.java
@@ -2,14 +2,12 @@ package com.abnamro.nl.channels.geninfo.bankmail.mappers;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
 import javax.inject.Named;
 import javax.inject.Singleton;
-import java.util.List;
 
 @Named
 @Singleton
@@ -26,9 +24,5 @@ public class MailResourceMapper {
 
     public <T> T map(String json, Class type) throws JsonProcessingException {
         return (T)mapper.readValue(json, type);
-    }
-
-    public <T> List<T> map(String json, TypeReference<List<T>> type) throws JsonProcessingException {
-        return mapper.readValue(json, type);
     }
 }
